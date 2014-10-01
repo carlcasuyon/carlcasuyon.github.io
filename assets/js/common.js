5bfe7aadee4d0b15981b72716a9499a93fffb8fe
carlcasuyon <ccasuyon@phmdpc0660.perfectfitgroup.local>
2014-10-01 13:25:46 +0800

    Adding new files

--- assets/js/common.js
+++ assets/js/common.js
@@ -11,11 +11,33 @@
 // Listen for the jQuery ready event on the document
    $(function() { // The DOM is ready!
     
-    $("#BSContainer").load("bs_bin/bs_content.html", function(){ // load brand store content
 
 
-		alert("test");
+
+   	//--- Container Align Center
+		var wr = $('#splash');
+		setWindow(wr);
+
+		$(window).resize(function() {
+		 	setWindow(wr);
+		});
+		//--- Container Align Center
+
+
+		//--- Container Align function
+		function setWindow(con){
+		   var div = $(con);
+		   var height = $(window).height();
+		   var width = $(window).width();
+		    div.css({
+		        'left' : width/2 - (div.width() / 2),  // half width - half element width
+		        'top' : height/2 - (div.height() / 2), // similar
+		        'z-index' : 15,                        // make sure element is on top
+		    });
+	}//--- Container Align function
+
 
 	}); // The DOM is end!
+
 // The rest of the code goes here!
 }));
