6604b0623cfc3f2790ddbc92028c0261579f3fde
carlcasuyon <ccasuyon@phmdpc0660.perfectfitgroup.local>
2014-10-01 10:55:26 +0800

    Adding new files

---
+++ assets/js/common.js
@@ -0,0 +1,19 @@
+// IIFE - Immediately Invoked Function Expression
+(function(yourcode) {
+
+// The global jQuery object is passed as a parameter
+	yourcode(window.jQuery, window, document);
+
+}(function($, window, document) {
+
+// The $ is now locally scoped 
+
+// Listen for the jQuery ready event on the document
+   $(function() { // The DOM is ready!
+    
+    alert("test");
+
+
+	}); // The DOM is end!
+// The rest of the code goes here!
+}));
